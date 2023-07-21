988133b98358d0e0f2985988d7d53d826c9b67bb
Farneser <farneser1@gmail.com>
2023-07-21 20:37:32 +0300

    feat: console render service map render

--- src/main/java/com/farneser/services/render/ConsoleRenderService.java
+++ src/main/java/com/farneser/services/render/ConsoleRenderService.java
@@ -1,14 +1,32 @@
 package com.farneser.services.render;
 
+import com.farneser.Coordinates;
 import com.farneser.Map;
+import com.farneser.entity.Entity;
 
-public class ConsoleRenderService extends RenderService{
+public class ConsoleRenderService extends RenderService {
     public ConsoleRenderService(Map map) {
         super(map);
     }
 
     @Override
     public void render(Map map) {
+        for (var x = 0; x < map.width; x++) {
+            for (var y = 0; y < map.width; y++) {
+                var coordinates = new Coordinates(x, y);
+                var entity = map.getEntityAt(coordinates);
+                var renderText = "";
 
+                if (entity == null){
+                    renderText = "... ";
+                }else{
+                    renderText = "ent ";
+                }
+
+                System.out.print(renderText);
+            }
+
+            System.out.println();
+        }
     }
 }
