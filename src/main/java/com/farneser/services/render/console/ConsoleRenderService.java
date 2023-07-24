df2074d95ce49850b11e308c0b8f11d6a8047562
Farneser <farneser1@gmail.com>
2023-07-24 23:01:50 +0300

    feat: console render random sprites
    fix build manifest

--- src/main/java/com/farneser/services/render/console/ConsoleRenderService.java
+++ src/main/java/com/farneser/services/render/console/ConsoleRenderService.java
@@ -5,33 +5,49 @@ import com.farneser.Map;
 import com.farneser.entity.Entity;
 import com.farneser.services.render.IRender;
 
+import java.util.Random;
+
 public class ConsoleRenderService implements IRender {
     public ConsoleRenderService() {
         super();
     }
 
+    private void cleanConsole(){
+        final var cleanString = "\033[H\033[2J";
+        System.out.print(cleanString);
+        System.out.flush();
+    }
+
     @Override
     public void render(Map map) {
+        final String combClear = "\033[H\033[2J";
+        System.out.print(combClear);
+        System.out.flush();
+
         for (var y = 0; y < map.height; y++) {
             for (var x = 0; x < map.width; x++) {
                 var coordinates = new Coordinates(x, y);
                 var entity = map.getEntityAt(coordinates);
-                System.out.print(selectUnicodeSpriteForEntity(entity) + " ");
+                System.out.print(selectUnicodeSpriteForEntity(entity) + "\t");
             }
 
             System.out.println();
         }
+
+        System.out.println();
     }
 
     private String selectUnicodeSpriteForEntity(Entity entity) {
         if (entity == null) return "..";
 
+        var random = new Random();
+
         return switch (entity.getClass().getSimpleName()) {
-            case "Grass" -> "\uD83C\uDF3F";
-            case "Rock" -> "\uD83E\uDEA8";
-            case "Tree" -> "\uD83C\uDF33";
-            case "Herbivore" -> "\uD83D\uDC14";
-            case "Predator" -> "\uD83D\uDC3A";
+            case "Grass" -> ConsoleSprites.GRASS[random.nextInt(ConsoleSprites.GRASS.length)];
+            case "Rock" -> ConsoleSprites.ROCK[random.nextInt(ConsoleSprites.ROCK.length)];
+            case "Tree" -> ConsoleSprites.TREE[random.nextInt(ConsoleSprites.TREE.length)];
+            case "Herbivore" -> ConsoleSprites.HERBIVORE[random.nextInt(ConsoleSprites.HERBIVORE.length)];
+            case "Predator" -> ConsoleSprites.Predator[random.nextInt(ConsoleSprites.Predator.length)];
             default -> "..";
         };
     }
---
+++ src/main/java/com/farneser/services/render/console/ConsoleSprites.java
@@ -0,0 +1,9 @@
+package com.farneser.services.render.console;
+
+class ConsoleSprites {
+    public final static String[] GRASS = {"\uD83C\uDF3F", "\uD83C\uDF3F"};
+    public final static String[] ROCK = {"\uD83E\uDEA8️"};
+    public final static String[] TREE = {"\uD83C\uDF33", "\uD83C\uDF32"};
+    public final static String[] HERBIVORE = {"\uD83D\uDC14", "\uD83D\uDC11"};
+    public final static String[] Predator = {"\uD83D\uDC3A", "\uD83E\uDD8A", "\uD83D\uDC05"};
+}
