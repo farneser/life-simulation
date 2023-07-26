f62136c0a3b7874c2fcd4e1498b4784511fee381
Farneser <farneser1@gmail.com>
2023-07-26 21:03:07 +0300

    fix: rename DefaultPathFinderService to BfsPathFinderService

---
+++ src/main/java/com/farneser/services/path_finder/BfsPathFinderService.java
@@ -0,0 +1,18 @@
+package com.farneser.services.path_finder;
+
+import com.farneser.Coordinates;
+import com.farneser.Map;
+
+import java.util.Deque;
+
+public class BfsPathFinderService extends PathFinderService {
+
+    public BfsPathFinderService() {
+        super();
+    }
+
+    @Override
+    public Deque<Coordinates> findPathTo(Map map, Coordinates from, Coordinates to) {
+        return null;
+    }
+}
