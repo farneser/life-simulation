189523fa8a0431c1036a722ca95e5649ac8367ab
Farneser <farneser1@gmail.com>
2023-08-10 13:36:20 +0300

    feat: breadth-first search testcases
    positive test shows how path correspond with correct
    
    negative test shows what happened if path not exits

---
+++ src/test/java/com/farneser/BfsPathFinderServiceTest.java
@@ -0,0 +1,83 @@
+package com.farneser;
+
+import com.farneser.entity.Entity;
+import com.farneser.entity.Grass;
+import com.farneser.entity.Tree;
+import com.farneser.entity.creature.Herbivore;
+import com.farneser.services.path_finder.bfs_path_finder.BfsPathFinderService;
+import com.farneser.services.render.console.ConsoleRenderService;
+import junit.framework.TestCase;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+
+public class BfsPathFinderServiceTest extends TestCase {
+
+    private final Map _map;
+
+    public BfsPathFinderServiceTest(String testName) {
+        super(testName);
+        var entities = new HashMap<Coordinates, Entity>();
+
+        _map = new Map(15, 15, new ConsoleRenderService());
+
+        _map.setEntityAt(new Coordinates(1, 0), new Tree(new Coordinates(0, 0)));
+        _map.setEntityAt(new Coordinates(0, 1), new Tree(new Coordinates(0, 0)));
+        _map.setEntityAt(new Coordinates(1, 1), new Tree(new Coordinates(0, 0)));
+
+        _map.setEntityAt(new Coordinates(0, 0), new Herbivore(new Coordinates(0, 0), new BfsPathFinderService(_map), _map));
+        _map.setEntityAt(new Coordinates(6, 0), new Herbivore(new Coordinates(0, 0), new BfsPathFinderService(_map), _map));
+
+        _map.setEntityAt(new Coordinates(5, 5), new Grass(new Coordinates(0, 0)));
+
+    }
+
+    private Deque<Coordinates> findPath(Coordinates from, Coordinates to) {
+        return _map.getCreatures().get(from).pathFinder.findPathTo(from, to);
+    }
+
+    public void testPositivePath() {
+        var path = findPath(new Coordinates(6, 0), new Coordinates(5, 5));
+
+        if (path == null) {
+            fail();
+            return;
+        }
+
+        var correctPath = new ArrayDeque<Coordinates>();
+        correctPath.add(new Coordinates(7, 1));
+        correctPath.add(new Coordinates(8, 2));
+        correctPath.add(new Coordinates(7, 3));
+        correctPath.add(new Coordinates(6, 4));
+
+        if (correctPath.size() != path.size()) {
+            fail();
+            return;
+        }
+
+        while (!path.isEmpty()) {
+            if (!path.pop().equals(correctPath.pop())) {
+                fail();
+                return;
+            }
+        }
+
+        assertTrue(true);
+
+    }
+
+    public void testNegativePath() {
+        _map.render();
+
+        var path = findPath(new Coordinates(0, 0), new Coordinates(5, 5));
+
+        if (path == null) {
+            assertTrue(true);
+        } else {
+            fail();
+        }
+
+    }
+
+}
