8157c07cff697a5a0a9dee3d62ab2d832925400a
farneser <farneser1@gmail.com>
2023-08-17 12:29:07 +0300

    feat: simple console render mode
    update: README.md

--- src/main/java/com/farneser/App.java
+++ src/main/java/com/farneser/App.java
@@ -4,7 +4,8 @@ import com.farneser.services.render.console.ConsoleRenderService;
 
 public class App {
     public static void main(String[] args) {
-        var simulation = new Simulation(new Map(30, 15, new ConsoleRenderService()));
+
+        var simulation = new Simulation(new Map(30, 15, new ConsoleRenderService(args.length > 0 && args[0].equals("-s"))));
 
         simulation.startSimulation();
 
